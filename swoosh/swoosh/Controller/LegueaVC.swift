dc558b04bdd683445398c89d43d1c03cbf9fd136
Антон Грищенко <angrishenko@MacBook-Air-Anton.local>
2018-03-17 00:27:19 +0500

    segues both variants

---
+++ swoosh/swoosh/Controller/LegueaVC.swift
@@ -0,0 +1,41 @@
+//
+//  LegueaVC.swift
+//  swoosh
+//
+//  Created by Антон Грищенко on 17.03.2018.
+//  Copyright © 2018 Антон Грищенко. All rights reserved.
+//
+
+import UIKit
+
+class LegueaVC: UIViewController {
+
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swoosh/swoosh/Controller/SkillVC.swift
@@ -0,0 +1,35 @@
+//
+//  SkillVC.swift
+//  swoosh
+//
+//  Created by Антон Грищенко on 17.03.2018.
+//  Copyright © 2018 Антон Грищенко. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
